0797918cab6646b8f5104f8402e9a9ae4bb23ff8
Rahul Bunker <rahulbunker6255@gmail.com>
2025-11-19 13:09:32 +0530

    Delete app/tools/tavily_search.py

--- app/tools/tavily_search.py
+++
@@ -1,7 +0,0 @@
-from langchain_community.tools.tavily_search import TavilySearchResults
-import os
-
-if 'TAVILY_API_KEY' not in os.environ:
-    raise ValueError("TAVILY_API_KEY environment variable not set.")
-
-tavily_search_tool = TavilySearchResults(name="Tavily Search")
