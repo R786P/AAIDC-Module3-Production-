c468b9204058e9c6e155ce94dd50291fe06d379c
Rahul Bunker <rahulbunker6255@gmail.com>
2025-11-19 12:09:39 +0530

    Update tavily_search.py

--- app/tools/tavily_search.py
+++ app/tools/tavily_search.py
@@ -1,1 +1,7 @@
+from langchain_community.tools.tavily_search import TavilySearchResults
+import os
 
+if 'TAVILY_API_KEY' not in os.environ:
+    raise ValueError("TAVILY_API_KEY environment variable not set.")
+
+tavily_search_tool = TavilySearchResults(name="Tavily Search")
